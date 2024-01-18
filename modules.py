25b0380d080657f9ec13edc5379483d3ded48024
Seyon Shanthan <seyontennis@gmail.com>
2024-01-18 10:00:18 +0100

    Working on the grid

--- modules.py
+++ modules.py
@@ -1,30 +1,43 @@
 import pygame as pg 
 import sys
-
+from settings import *
 
 
 class Grid:
     def __init__(self):
         self.rows = 10
         self.columns = 10
-        self.grid = self.create_grid()
+        self.grid_nett = self.create_grid()
     def create_grid(self):
         gridnett = []
         for i in range(self.rows):
-            col = self.columns*[0]
+            block = Block(100*i,0)
+            col = self.columns*[block]
             gridnett.append(col)
         return gridnett
     def show_grid(self):
-        for i in self.grid:
-            print(i)
+        for i in self.grid_nett:
+            for j in i:
+                print(j.x, j.y)
+                print("----------")
 
 
 class Block:
-    ...
+    def __init__(self, x,y,width=50, height=100, color="white"):
+        self.x = x
+        self.y = y 
+        self.width = width 
+        self.height = height
+        self.color = color 
+    def draw(self):
+        block = pg.Rect(self.x, self.y,self.width, self.height)
+        pg.draw.rect(WIN, self.color, block)
 
 
 
+grid = Grid()
 
+print(grid.show_grid())
 
 
 
